45e988ac83dca9cbcaa5d2b263fc901ec1d6e4f1
iotegui <mati_otegui@hotmail.com>
2015-06-07 12:06:22 -0300

    nuevo test sobre Centro de mineral que testea excepcionRecursos Insuficientes

--- test/edificiosTerranTests/CentroDeMineralTest.java
+++ test/edificiosTerranTests/CentroDeMineralTest.java
@@ -6,7 +6,9 @@ import java.awt.Color;
 import java.io.IOException;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import juego.Juego;
 import juego.excepciones.*;
@@ -21,20 +23,27 @@ import juego.razas.*;
 public class CentroDeMineralTest {
 	
 	@Before 
-	public void iniciarJuego() throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException {
+	public void reiniciarJuego() throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException {
 		
+		Juego.getInstance().reiniciar();
 		Juego juego = Juego.getInstance(); 
 		
 		juego.crearJugador("jugadorTerran", new Terran(), Color.red);
 		juego.crearJugador("jugadorProtoss", new Protoss(), Color.blue);
 		
-		//Se genera un mapa, se inicializan los datos, etc...
 		juego.iniciarJuego();
+		
 	}
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 	
 	@Test
 	public void testJugadorTerranCreaCentroDeMineralEnNodoDeMineralesSatisfactoriamente() 
-			throws RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada {
+			throws RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada, 
+			ColorInvalido, NombreInvalido, FaltanJugadores, IOException {
+		
+		this.reiniciarJuego();
 		
 		Juego juego = Juego.getInstance();
 		
@@ -82,6 +91,36 @@ public class CentroDeMineralTest {
 		 * Por turno recolecta, en un nodo con recursos,
 		 * un total de 10 minerales.
 		 */
+		
+	}
+	
+	@Test
+	public void testSiJugadorTerranNoPoseeSuficientesRecursosParaConstruirErrorRecursosInsuficientes() 
+			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, RecursosInsuficientes, 
+			UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada {
+		
+		this.reiniciarJuego();
+		
+		Juego juego = Juego.getInstance();
+		
+		Jugador jugadorActual = juego.turnoDe();
+		
+		//Esto es simplemente para asegurarme que estoy testeando sobre el jugador de raza terran
+		//No existe ningun metodo similar implementado en el juego.
+		if (!jugadorActual.suNombreEs("jugadorTerran")) { 
+			jugadorActual.finalizarTurno();
+			jugadorActual = juego.turnoDe();
+		}
+		
+		//El centro de mineral vale 50, si gasto 160 de los 200 iniciales le quedan 40 minerales.
+		jugadorActual.consumirMinerales(160);
+		
+		/* El centro de mineral se crea alrededor de las coordenadas centrales especificadas (x,y) 
+		 * si existe un nodo de minerales y no esta ocupado por ninguna construccion propia o enemiga.
+		 * En la coordenada (0,0) del mapa 'test' existe un nodo mineral, por lo que es correcto crearlo en esta ubicacion.
+		 */
+		exception.expect(RecursosInsuficientes.class);
+		jugadorActual.construir(new ConstructorCentroDeMineral(), new Coordenada(0,0));
 		
 	}
 	
