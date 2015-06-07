bdfdb52f394c932d90b30dd5a576fe1a8b397cfa
iotegui <mati_otegui@hotmail.com>
2015-06-07 17:28:19 -0300

    modificacion en test de generador mapa para que chequee si hay recursos tambien

--- test/mapa/GeneradorMapaTest.java
+++ test/mapa/GeneradorMapaTest.java
@@ -16,19 +16,23 @@ public class GeneradorMapaTest {
 
 	@Test
 	public void testCreacionDeCentroMapaAPartirDeArchivoParseaCorrectamente() throws IOException, CoordenadaFueraDeRango {
-		//primero tengo que incializar un mapa
 		
+		// Debe ingresarse la ubicacion del mapa que se va a utilizar.
 		GeneradorMapa generadorMapa = new GeneradorMapa();
 		Mapa mapa = generadorMapa.obtenerMapa("mapas/test.map");
 		
-		
+		// El mapa test posee las primeras tres celdas de tierra y la cuarta de aire		
 		assertEquals(Material.tierra, mapa.obtenerCelda(new Coordenada(0, 0)).obtenerMaterial());
 		assertEquals(Material.tierra, mapa.obtenerCelda(new Coordenada(1, 0)).obtenerMaterial());
 		assertEquals(Material.tierra, mapa.obtenerCelda(new Coordenada(2, 0)).obtenerMaterial());
 		assertEquals(Material.aire, mapa.obtenerCelda(new Coordenada(3, 0)).obtenerMaterial());
-		assertEquals(Material.tierra, mapa.obtenerCelda(new Coordenada(4, 0)).obtenerMaterial());
 				
-		assertFalse(mapa.obtenerCelda(new Coordenada(0, 0)).obtenerRecurso().estaAgotado());
+		// El mapa test posee en la primera celda un nodo de minerales y en la segunda uno de gas vespeno,
+		// en la tercera y cuarta no posee recursos.
+		assertTrue(mapa.obtenerCelda(new Coordenada(0, 0)).poseeRecursos());
+		assertTrue(mapa.obtenerCelda(new Coordenada(1, 0)).poseeRecursos());
+		assertFalse(mapa.obtenerCelda(new Coordenada(2, 0)).poseeRecursos());
+		assertFalse(mapa.obtenerCelda(new Coordenada(3, 0)).poseeRecursos());
 		
 	}
 
