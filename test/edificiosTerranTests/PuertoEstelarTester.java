b185ba020b0fbb12979e300378084ef06f1552b1
iotegui <mati_otegui@hotmail.com>
2015-06-09 16:28:20 -0300

    test generales de puerto estelar pasando, faltan casos particulares

--- test/edificiosTerranTests/PuertoEstelarTester.java
+++ test/edificiosTerranTests/PuertoEstelarTester.java
@@ -11,7 +11,7 @@ import juego.excepciones.ColorInvalido;
 import juego.excepciones.FaltanJugadores;
 import juego.excepciones.NombreInvalido;
 import juego.interfaces.Controlable;
-import juego.interfaces.commandConstrucciones.militares.ConstructorFabrica;
+import juego.interfaces.commandConstrucciones.militares.ConstructorPuertoEstelar;
 import juego.interfaces.excepciones.CeldaOcupada;
 import juego.interfaces.excepciones.ConstruccionesNoSeMueven;
 import juego.interfaces.excepciones.ImposibleConstruir;
@@ -31,7 +31,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class FabricaTester {
+public class PuertoEstelarTester {
 
 	@Before 
 	public void reiniciarJuego() throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException {
@@ -50,7 +50,7 @@ public class FabricaTester {
 	public ExpectedException exception = ExpectedException.none();
 	
 	@Test
-	public void testCreacionDeFabricaSatisfactoria() 
+	public void testCreacionDePuertoEstelarSatisfactoria() 
 			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, RecursosInsuficientes,
 			UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada {
 		
@@ -60,7 +60,7 @@ public class FabricaTester {
 		Mapa mapa = juego.getMapa();
 		Coordenada coord = new Coordenada(0,1);
 		
-		/* El rango de celdas de una fabrica debe ser de seis
+		/* El rango de celdas de un puerto estelar debe ser de seis
 		 * teniendo como coordenada determinante a la ingresada.
 		 * Si la misma es, por ejemplo, (0,0) el deposito ocupara las celdas
 		 * (0,0), (1,0), (0,1), y (1,1). 
@@ -74,17 +74,17 @@ public class FabricaTester {
 		 * - - - - - -
 		 */
 		
-		// Necesita 100 para construir la fabrica, este metodo no se debe usar,
+		// Necesita 100 de gas vespeno para construir la fabrica, este metodo no se debe usar,
 		// sirve para los test y para los recolectores.
 		jugadorActual.recolectarGasVespeno(100);
 		
 		//En el mapa 'test' la coordenada (0,1) es una coordenada valida para crear la barraca
-		jugadorActual.construir(new ConstructorFabrica(), new Coordenada(0,1));
+		jugadorActual.construir(new ConstructorPuertoEstelar(), new Coordenada(0,1));
 		
 		// El metodo 'puedeConstruirMarine' verifica unicamente si hay una barraca activa,
 		// no tiene en cuenta el costo mineral de construir un marine
 		
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i < 9; i++) {
 			jugadorActual.finalizarTurno();
 			jugadorActual = juego.turnoDe();
 			if (jugadorActual.suNombreEs("jugadorTerran")) {
@@ -108,10 +108,10 @@ public class FabricaTester {
 		Juego juego = Juego.getInstance();
 		Jugador jugadorActual = juego.turnoDe();
 		
-		//La fabrica vale 200 minerales y 100 de gas vespeno, si no recolecto gas vespeno no podra construir.
+		//La fabrica vale 150 minerales y 100 de gas vespeno, si no recolecto gas vespeno no podra construir.
 		
 		exception.expect(RecursosInsuficientes.class);
-		jugadorActual.construir(new ConstructorFabrica(), new Coordenada(0,1));
+		jugadorActual.construir(new ConstructorPuertoEstelar(), new Coordenada(0,1));
 		
 	}
 	
@@ -127,7 +127,7 @@ public class FabricaTester {
 		//Coloco una coordenada negativa, ya que los mapas no tienen un limite fijo, pero
 		//si es negativa seguro no debe existir.
 		exception.expect(CoordenadaFueraDeRango.class);
-		jugadorActual.construir(new ConstructorFabrica(), new Coordenada(-10,3));
+		jugadorActual.construir(new ConstructorPuertoEstelar(), new Coordenada(-10,3));
 		
 	}
 	
@@ -140,34 +140,15 @@ public class FabricaTester {
 		Jugador jugadorActual = juego.turnoDe();
 		
 		jugadorActual.recolectarGasVespeno(100);
-		jugadorActual.construir(new ConstructorFabrica(), new Coordenada(0,1));
+		jugadorActual.construir(new ConstructorPuertoEstelar(), new Coordenada(0,1));
 		
 		exception.expect(CeldaOcupada.class);
-		jugadorActual.construir(new ConstructorFabrica(), new Coordenada(0,1));
+		jugadorActual.construir(new ConstructorPuertoEstelar(), new Coordenada(0,1));
 		
 	}
 	
 	@Test
-	public void testSiUnProtossIntentaConstruirUnaFabricaErrorImposibleConstruir() 
-			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, 
-			RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada {
-		
-		this.reiniciarJuego();
-		Juego juego = Juego.getInstance();
-		Jugador jugadorActual = juego.turnoDe();
-		
-		if (!jugadorActual.suNombreEs("jugadorProtoss")) { 
-			jugadorActual.finalizarTurno();
-			jugadorActual = juego.turnoDe();
-		}
-		
-		exception.expect(ImposibleConstruir.class);
-		jugadorActual.construir(new ConstructorFabrica(), new Coordenada(0,1));
-		
-	}
-	
-	@Test
-	public void testSiUnJugadorEsPropietarioDeUnaFabricaEsUnRecolectorAliado() 
+	public void testSiUnJugadorEsPropietarioDeUnPuertoEstelarEsUnRecolectorAliado() 
 			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, 
 			RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada {
 		
@@ -179,9 +160,9 @@ public class FabricaTester {
 		Jugador jugadorActual = juego.turnoDe();
 		
 		jugadorActual.recolectarGasVespeno(100);
-		jugadorActual.construir(new ConstructorFabrica(), coord);
+		jugadorActual.construir(new ConstructorPuertoEstelar(), coord);
 
-		for (int i = 0; i < 12; i++) {		
+		for (int i = 0; i < 10; i++) {		
 			jugadorActual.finalizarTurno();
 			jugadorActual = juego.turnoDe();		
 		}
@@ -194,7 +175,7 @@ public class FabricaTester {
 	}
 	
 	@Test
-	public void testSiUnJugadorNoEsPropietarioDeUnaFabricaEsUnRecolectorEnemigo() 
+	public void testSiUnJugadorNoEsPropietarioDeUnPuertoEstelarEsUnRecolectorEnemigo() 
 			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, 
 			RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango, CeldaOcupada {
 		
@@ -206,7 +187,7 @@ public class FabricaTester {
 		Jugador jugadorActual = juego.turnoDe();
 		
 		jugadorActual.recolectarGasVespeno(100);
-		jugadorActual.construir(new ConstructorFabrica(), coord);
+		jugadorActual.construir(new ConstructorPuertoEstelar(), coord);
 
 		for (int i = 0; i < 11; i++) {		
 			jugadorActual.finalizarTurno();
@@ -221,7 +202,7 @@ public class FabricaTester {
 	}
 	
 	@Test
-	public void testSiUnJugadorProtossTrataDeMoverUnaFabricaTerranErrorPropietarioInvalido() 
+	public void testSiUnJugadorProtossTrataDeMoverUnPuertoEstelarTerranErrorPropietarioInvalido() 
 			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, 
 			RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango,
 			CeldaOcupada, ConstruccionesNoSeMueven, PropietarioInvalido {
@@ -234,7 +215,7 @@ public class FabricaTester {
 		Jugador jugadorActual = juego.turnoDe();
 		
 		jugadorActual.recolectarGasVespeno(100);
-		jugadorActual.construir(new ConstructorFabrica(), coord);
+		jugadorActual.construir(new ConstructorPuertoEstelar(), coord);
 
 		for (int i = 0; i < 11; i++) {
 		
@@ -252,7 +233,7 @@ public class FabricaTester {
 	}
 	
 	@Test
-	public void testSiUnJugadorTerranTrataDeMoverUnaFabricaErrorConstruccionesNoSeMueven() 
+	public void testSiUnJugadorTerranTrataDeMoverUnPuertoEstelarErrorConstruccionesNoSeMueven() 
 			throws ColorInvalido, NombreInvalido, FaltanJugadores, IOException, 
 			RecursosInsuficientes, UbicacionInvalida, ImposibleConstruir, CoordenadaFueraDeRango,
 			CeldaOcupada, ConstruccionesNoSeMueven, PropietarioInvalido {
@@ -265,7 +246,7 @@ public class FabricaTester {
 		Jugador jugadorActual = juego.turnoDe();
 
 		jugadorActual.recolectarGasVespeno(100);
-		jugadorActual.construir(new ConstructorFabrica(), coord);
+		jugadorActual.construir(new ConstructorPuertoEstelar(), coord);
 
 		for (int i = 0; i < 11; i++) {		
 			jugadorActual.finalizarTurno();
