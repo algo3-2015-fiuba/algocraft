636f7d5f37653b6d1aa32f11f4ce0d2a30182051
Daniel Sorichetti <me@dancodes.com.ar>
2015-06-17 12:35:07 -0300

    Agrego pruebas de mapa

--- test/mapa/mapaTest.java
+++ test/mapa/mapaTest.java
@@ -4,14 +4,22 @@ import static org.junit.Assert.*;
 
 import java.awt.Color;
 import java.io.IOException; 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import juego.Juego;
 import juego.excepciones.ColorInvalido;
 import juego.excepciones.FaltanJugadores;
 import juego.excepciones.NombreInvalido;
 import juego.jugadores.JugadorProtoss;
 import juego.jugadores.JugadorTerran;
+import juego.mapa.Celda;
+import juego.mapa.Coordenada;
+import juego.mapa.Mapa;
+
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class mapaTest {
@@ -43,4 +51,34 @@ public class mapaTest {
 	
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void testSeleccionaRadialmenteCeldasCorrectamente() {
+		this.reiniciarJuego();
+		
+		Mapa mapa = Juego.getInstance().getMapa();
+		
+		Coordenada centro = new Coordenada(5, 5);
+		
+		Collection<Celda> celdasSeleccionadas = mapa.obtenerRadialmenteRangoDeCeldasDisponibles(centro, 1);
+		
+		/*    
+		 *    ----O----
+		 *    ---OOO---
+		 *    ----O----
+		 */
+		assertEquals(celdasSeleccionadas.size(), 5);
+		
+		Collection<Celda> celdasSeleccionadas2 = mapa.obtenerRadialmenteRangoDeCeldasDisponibles(centro, 2);
+		
+		/*    
+		 * 	  -----O----
+		 *    ----OOO---
+		 *    ---OOOOO---
+		 *    ----OOO---
+		 *    -----O----
+		 */
+		assertEquals(celdasSeleccionadas2.size(), 13);
+		
+	}
 }
